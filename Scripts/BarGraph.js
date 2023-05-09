d27a49b5f2244e29515ab5871b4a29be9f883fad
Albithealbino <alexkmclachlan@gmail.com>
2023-05-09 11:08:21 +1000

    Update BarGraph.js

--- Scripts/BarGraph.js
+++ Scripts/BarGraph.js
@@ -4,6 +4,8 @@
 <script src="https://d3js.org/d3.v4.js"></script>
 </head>
 <body>
+
+
 <!-- Create a div where the graph will take place -->
 <div id="my_dataviz"></div>
 <script>
@@ -13,7 +15,7 @@ const colours = ['#ececa3', '#b5e550', '#abc32f', '#809c13', '#607c3c'];
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 90, left: 40},
     width = 460 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+    height = 460 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -26,7 +28,6 @@ var svg = d3.select("#my_dataviz")
 
 // Parse the Data
 d3.csv("https://raw.githubusercontent.com/Albithealbino/WebPortfolio/main/Data/WhyYouShouldHireMe.csv", function(data) {
-    
 
 // X axis
 var x = d3.scaleBand()
@@ -39,13 +40,21 @@ svg.append("g")
   .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
+    
+
+// Add X axis label:
+svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", 250)
+    .attr("y", height + margin.top + 50)
+    .text("Promotions each year");
 
 // Add Y axis
 var y = d3.scaleLinear()
-  .domain([0, 5])
+  .domain([0, 3])
   .range([ height, 0]);
 svg.append("g")
-  .call(d3.axisLeft(y));
+  .call(d3.axisLeft(y).tickFormat(d3.format('d')));
 
 // Bars
 svg.selectAll("mybar")
@@ -54,7 +63,7 @@ svg.selectAll("mybar")
   .append("rect")
     .attr("x", function(d) { return x(d.Year); })
     .attr("width", x.bandwidth())
-    .attr("fill", function(d, i) {console.log; return colours[i]})
+    .attr("fill", function(d, i) {console.log(i); return colours[i]})
     // no bar at the beginning thus:
     .attr("height", function(d) { return height - y(0); }) // always equal to 0
     .attr("y", function(d) { return y(0); })
